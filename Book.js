7fc267f7d1ffe243ff665ae361e2a10b38d9dcc0
maxhandley97 <maxhandley97@gmail.com>
2024-06-26 12:15:27 +1000

    added read toggle and delete buttons to simplify processes

--- Book.js
+++ Book.js
@@ -22,11 +22,38 @@ function addBookToLibrary(book) {
     displayLibrary()
 }
 
+function deleteBook (index) {
+    library.splice(index, 1)
+    displayLibrary()
+}
+
+function updateBookRead (index) {
+    const book = library[index]
+    book.read = book.read === 'read already' ? 'not read yet' : 'read already';
+    displayLibrary()
+}
+
 function displayLibrary() {
     const libraryOutput = document.getElementById('libraryOutput')
+    libraryOutput.innerHTML = ''
     library.forEach((book, index) => {
-        const bookInfo = document.createElement('ul')
-        bookInfo.innerText = `${index + 1}. ${book.info()}`
+        const bookInfo = document.createElement('div')
+
+        const bookTitle = document.createElement('p')
+        bookTitle.innerText = `${index + 1}. ${book.info()}`
+        bookInfo.appendChild(bookTitle)
+
+        const bookDelete = document.createElement('button')
+        bookDelete.innerText = "Remove"
+        bookDelete.addEventListener('click', () => {deleteBook(index)})
+        bookInfo.appendChild(bookDelete)
+
+
+        const bookRead = document.createElement('button')
+        bookRead.innerText = "Toggle Read"
+        bookRead.addEventListener('click', () => updateBookRead(index))
+        bookInfo.appendChild(bookRead)
+
         libraryOutput.appendChild(bookInfo)
     })
 
@@ -48,7 +75,6 @@ function displayLibrary() {
     })
 
 }
-
 
 
 const theHobbit = new Book('the Hobbit', 'J.R.R. Tolkien', 295, false)
